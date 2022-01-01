bd1edd0eff6f5a3cf16265664eb3577aee4cc5d8
Valeria <vmoraleshuaman@gmail.com>
2022-01-01 13:10:55 +1030

    class day 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country = 'Peru';
+let continent = 'America';
+let population = '30 millions';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(isIsland)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+//VALUES AND VARIABLES
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+//VALUES - piece of data, most fundamental unit in programming
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas"; //Declaring a variable (in a pc memory)
+console.log(firstName); // Now we can use this variable many times, change value only in one place
+
+// RULES FOR NAMING VARIABLES
+
+//Camelcase firstNamePerson, don't start with an uppercase
+//first_name_person (ruby)
+//Don't start with a number, only $ or _
+//Don't use a reserved keyword in JS (new,name)
+
+let myFirstJob = 'Programmer'; // not job1
+let myCurrentJob = 'Teacher'
+
+console.log(myFirstJob);
+
+//DATA TYPE
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas'); // string always with quotes
+
+javascriptIsFun = 'YES!'; //Dynamic type (without the let)
+console.log(typeof javascriptIsFun);
+
+*/
+
+//LET CONST VAR
+
+let age = 30;
+age = 31; // reassigning value, remuteted
+
+const birthYear = 1991; // const can't change -  reassign
+//best practice to not changing variables too much to avoid bugs or errors
+
+var job = 'programmer'; //avoid VAR is old
+job = 'teacher';
+
+//Always declare variables
